27424c001bb4d3884106ce26f027778debec6aba
siddesh_paidcoder <siddeshwar.madargave@paidcoders.com>
2026-02-17 12:01:47 +0530

    loading issue commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,43 +6,29 @@ import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
 import { useThemeStore } from "./store/useThemeStore";
 import { useEffect } from "react";
-
-import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
   const { theme } = useThemeStore();
-
-  const location = useLocation();
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
-
-  // Don't block rendering of public routes while the backend wakes up (common on free tiers).
-  const publicRoutes = new Set(["/login", "/signup"]);
-  const isPublicRoute = publicRoutes.has(location.pathname);
-  const shouldBlockForAuthCheck = isCheckingAuth && !authUser && !isPublicRoute;
-
-  if (shouldBlockForAuthCheck) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <Loader className="size-10 animate-spin" />
-      </div>
-    );
-  }
 
   return (
     <div data-theme={theme}>
       <Navbar />
 
       <Routes>
-        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : isCheckingAuth ? <Navigate to="/login" /> : <Navigate to="/login" />}
+        />
         <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
         <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
         <Route path="/settings" element={<SettingsPage />} />
